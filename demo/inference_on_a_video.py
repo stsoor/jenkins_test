58a286d163082320881a5b85d1ed71a4d40a5644
Bence Sarkadi-Nagy <stsoor@gmail.com>
2024-08-31 02:25:15 +0200

    trying mjpg instead of xvid

--- demo/inference_on_a_video.py
+++ demo/inference_on_a_video.py
@@ -46,7 +46,7 @@ def annotate_video(input_video_path, output_dir, model,
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     total_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
     video_extension = ".avi"
